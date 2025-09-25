d7ad249dc66777ca67d448b793151ff3ae2be5eb
bhsohn <hanmuson1@gmail.com>
2025-09-25 10:43:41 +0900

    facing 관련 처리

--- LittleSword/Assets/02_Scripts/Network/NetworkPlayer.cs
+++ LittleSword/Assets/02_Scripts/Network/NetworkPlayer.cs
@@ -16,6 +16,16 @@ public class NetworkPlayer : NetworkBehaviour
     private InputHandler inputHandler;
     private CinemachineCamera cmCamera;
 
+    #region 네트워크 변수
+    private NetworkVariable<bool> networkIsFacingRight = new NetworkVariable<bool>
+        (
+        true,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner
+        );
+
+    #endregion
+
     #region 유니티 이벤트
     private void Awake()
     {
@@ -26,11 +36,34 @@ public class NetworkPlayer : NetworkBehaviour
         inputHandler = GetComponent<InputHandler>();
         cmCamera = FindFirstObjectByType<CinemachineCamera>();
     }
+    private void Start()
+    {
+        if (IsOwner)
+        {
+            inputHandler.OnMove += HandleMove;
+        }
+    }
+
+    private void HandleMove(Vector2 ctx)
+    {
+        // 현재 스프라이트의 방향 저장
+        bool currentFacingRight = !spriteRenderer.flipX;
+
+        // 방향이 변경 되었을 때 네트워크 변수를 업데이트
+        if (networkIsFacingRight.Value != currentFacingRight)
+        {
+            networkIsFacingRight.Value = currentFacingRight;
+        }
+    }
+
     #endregion
 
     #region 네트워크 이벤트
     public override void OnNetworkSpawn()
     {
+        // 네트워크 변수 이벤트 연결
+        networkIsFacingRight.OnValueChanged += OnFacingRightChanged;
+
         if (IsOwner)
         {
             cmCamera.Follow = this.transform;
@@ -43,6 +76,15 @@ public class NetworkPlayer : NetworkBehaviour
             basePlayer.enabled = false;
         }
     }
+
+    private void OnFacingRightChanged(bool previousValue, bool newValue)
+    {
+        if (!IsOwner)
+        {
+            spriteRenderer.flipX = !newValue;
+        }
+    }
+
     public override void OnNetworkDespawn()
     {
 
